d0022867dd71e0a9a0b61bff91b1c833ac2ca848
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-24 00:58:20 -0600

    Added bodystatdelete template

--- bodystatdelete.php
+++ bodystatdelete.php
@@ -8,25 +8,20 @@ include "./utilities/getUID.php";
 
 $placeholder = array(
     "date" => "2022-02-02",
-    "bodyweight" => 145,
-    "user_measurement" => 34.02,
     "user_stat" => "Chest"
 );
-
 
 $uid = getUID();
 
 if ($uid !== false) {
     try {
-        $stmt = $conn->prepare("INSERT INTO bodystats (user_id, entry_date, body_weight, user_defined_measurement, user_defined_stat)
-            VALUES (:uid, :entry_date, :bodyweight, :user_measurement, :user_stat)");
+        $stmt = $conn->prepare("DELETE FROM bodystats 
+            WHERE user_id = :uid AND entry_date = :entry_date AND user_defined_stat = :user_stat");
         $stmt->bindParam(":uid", $uid);
         $stmt->bindParam(":entry_date", $placeholder['date']);
-        $stmt->bindParam(":bodyweight", $placeholder['bodyweight']);
-        $stmt->bindParam(":user_measurement", $placeholder['user_measurement']);
         $stmt->bindParam(":user_stat", $placeholder['user_stat']);
         $stmt->execute();
-        echo "success";
+        echo "Success";
     } catch (PDOException $e) {
         echo $e->getMessage();
     }
