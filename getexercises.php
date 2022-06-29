74a7d34644c31b9abe5fdbd30bf2eefbbf5bf5b8
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-06-28 21:42:26 -0600

    added getExercises

---
+++ getexercises.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include "./utilities/standardizedResponse.php";
+include "./connect.php";
+include "./utilities/getUID.php";
+include "./utilities/replyAfterQueries.php";
+
+$uid = getUID();
+
+if ($uid !== false){
+    try {
+        $stmt = $conn->prepare("SELECT exercise FROM exercises WHERE user_id = :uid");
+        $stmt->bindParam(":uid", $uid);
+        $stmt->execute();
+        standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_COLUMN));
+    } catch (Exception $e){
+        standardizedResponse($e->getMessage());
+    }
+}
