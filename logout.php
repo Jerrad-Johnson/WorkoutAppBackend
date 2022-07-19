7cc02534fef50d52a647a032a49c8b08770d4364
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-07-19 15:54:02 -0600

    log out script

--- logout.php
+++ logout.php
@@ -1,8 +1,19 @@
 <?php
 session_start();
+include "./utilities/standardizedResponse.php";
+
+if ($_SESSION['username'] === null) {
+    $data["loggedout"] = "true";
+    return standardizedResponse("Already logged out.", $data);
+}
 
 $_SESSION['authenticated'] = false;
 $_SESSION['username'] = null;
+
+if ($_SESSION['username'] === null) {
+    $data["loggedout"] = "true";
+    return standardizedResponse("Logged out.", $data);
+}
 
 ?>
 
