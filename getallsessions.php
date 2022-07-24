c33b54d092f0e3d02cd8cb5cabf5b740edfad2f3
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-07-24 13:55:19 -0600

    Moved file to correct folder.

--- getallsessions.php
+++ getallsessions.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 include "./utilities/standardizedResponse.php";
 include "./connect.php";
@@ -11,8 +10,7 @@ $sessionToFind = json_decode(file_get_contents('php://input'));
 
 if ($uid !== false) {
     try {
-        $stmt = $conn->prepare("SELECT session_date, session_title FROM sessions 
-            WHERE user_id = :uid");
+        $stmt = $conn->prepare("SELECT DISTINCT session_date, session_title FROM sessions WHERE user_id = :uid LIMIT 100"); //TODO Allow user to select range (limit)
         $stmt->bindParam(":uid", $uid);
         $stmt->execute();
         standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_ASSOC));
