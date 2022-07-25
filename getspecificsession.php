dc89901b6131b8a6fe87be88dd3a30ad34b64a9c
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-07-24 18:06:42 -0600

    bug fix in logout script. Username set check was faulty.

--- getspecificsession.php
+++ getspecificsession.php
@@ -7,6 +7,7 @@ include "./utilities/replyAfterQueries.php";
 
 $uid = getUID();
 $sessionToFind = json_decode(file_get_contents('php://input'));
+echo $sessionToFind;
 
 if ($uid !== false){
     try {
--- logout.php
+++ logout.php
@@ -2,18 +2,21 @@
 session_start();
 include "./utilities/standardizedResponse.php";
 
-if ($_SESSION['username'] === null) {
-    $data["loggedout"] = "true";
-    return standardizedResponse("Already logged out.", $data);
+try {
+    if (!isset($_SESSION['username'])){
+        $data["loggedout"] = "true";
+        return standardizedResponse("Already logged out.", $data);
+    }
+
+    $_SESSION['authenticated'] = false;
+    $_SESSION['username'] = null;
+
+    if ($_SESSION['username'] === null) {
+        $data["loggedout"] = "true";
+        return standardizedResponse("Logged out.", $data);
+    }
+} catch (Exception $e) {
+    echo $e;
 }
-
-$_SESSION['authenticated'] = false;
-$_SESSION['username'] = null;
-
-if ($_SESSION['username'] === null) {
-    $data["loggedout"] = "true";
-    return standardizedResponse("Logged out.", $data);
-}
-
 ?>
 
