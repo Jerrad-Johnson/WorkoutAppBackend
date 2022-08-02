92d442434fd1ebd1a0cc2bc3d0bbcec12a2df419
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-02 02:07:59 -0600

    added createaccount email validation

--- createaccount.php
+++ createaccount.php
@@ -3,8 +3,12 @@ include "./connect.php";
 include "./utilities/standardizedResponse.php";
 
 $entry = json_decode(file_get_contents('php://input'));
-
 $hashedPassword = password_hash($entry->password, PASSWORD_DEFAULT);
+
+if (!filter_var($entry->email, FILTER_VALIDATE_EMAIL)){
+    standardizedResponse("Invalid e-mail address.");
+    return;
+}
 
 try {
     $sth = $conn->prepare("INSERT INTO users (username, password, email) VALUES(:user, :pass, :email)");
