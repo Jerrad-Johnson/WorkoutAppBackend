056af1190da92f09f78c6a9766cd6d606c6bbfe5
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-08 19:18:40 -0600

    query corrections

--- getsessiondataforonermcalculation.php
+++ getsessiondataforonermcalculation.php
@@ -6,11 +6,13 @@ include "./utilities/getUID.php";
 include "./utilities/replyAfterQueries.php";
 
 $uid = getUID();
+$exercise = json_decode(file_get_contents('php://input'));
 
 if ($uid !== false){
     try {
-        $stmt = $conn->prepare("SELECT session_date, session_title, exercise, weight_lifted, reps FROM sessions WHERE user_id = :uid");
+        $stmt = $conn->prepare("SELECT session_date, session_title, exercise, weight_lifted, reps FROM sessions WHERE user_id = :uid AND exercise = :exercise");
         $stmt->bindParam(":uid", $uid);
+        $stmt->bindParam(":exercise", $exercise);
         $stmt->execute();
         standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_COLUMN));
     } catch (Exception $e){
