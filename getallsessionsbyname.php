6676b83980367d4d7756dd72c0a25f0a7885b6ac
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-13 14:08:56 -0600

    updated order by to DESC

--- getallsessionsbyname.php
+++ getallsessionsbyname.php
@@ -11,8 +11,9 @@ $sessionToFind = json_decode(file_get_contents('php://input'));
 if ($uid !== false) {
     try {
         $stmt = $conn->prepare("SELECT session_date, session_title, exercise, weight_lifted, reps FROM sessions 
-            WHERE user_id = :uid AND session_title = :title ORDER BY session_date");
+            WHERE user_id = :uid AND session_title = :title ORDER BY session_date DESC");
         $stmt->bindParam(":uid", $uid);
+        $stmt->bindParam(":title", $sessionToFind);
         $stmt->execute();
         standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_ASSOC));
     } catch (Exception $e) {
