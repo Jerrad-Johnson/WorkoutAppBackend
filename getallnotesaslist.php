4906d6a2bb20d9fedcfa8536a3865ef658266c94
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-21 14:46:07 -0600

    added getallnotesaslist.php

---
+++ getallnotesaslist.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include "./utilities/standardizedResponse.php";
+include "./connect.php";
+include "./utilities/getUID.php";
+include "./utilities/replyAfterQueries.php";
+
+$uid = getUID();
+$sessionToFind = json_decode(file_get_contents('php://input'));
+
+if ($uid !== false) {
+    try {
+        $stmt = $conn->prepare("SELECT session_date, session_title, notes FROM usersessionnotes 
+            WHERE user_id = :uid ORDER BY session_date DESC");
+        $stmt->bindParam(":uid", $uid);
+        $stmt->execute();
+        standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_ASSOC));
+    } catch (Exception $e) {
+        standardizedResponse($e->getMessage());
+    }
+} else {
+    standardizedResponse("Cannot find user; try logging in again.");
+}
+
+?>
