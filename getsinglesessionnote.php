f28e7d6dc8731924bb15c77be6396db5e37f583b
Jerrad-Johnson <j_johnson21@mail.fhsu.edu>
2022-08-21 19:22:01 -0600

    added getsinglesessionnote.php

--- getsinglesessionnote.php
+++ getsinglesessionnote.php
@@ -6,15 +6,15 @@ include "./utilities/getUID.php";
 include "./utilities/replyAfterQueries.php";
 
 $uid = getUID();
-$sessionToFind = json_decode(file_get_contents('php://input'));
+$id = json_decode(file_get_contents('php://input'));
 
 if ($uid !== false) {
     try {
-        $stmt = $conn->prepare("SELECT session_date, session_title, id FROM usersessionnotes 
-            WHERE user_id = :uid ORDER BY session_date DESC");
+        $stmt = $conn->prepare("SELECT notes FROM usersessionnotes WHERE user_id = :uid AND id = :id");
         $stmt->bindParam(":uid", $uid);
+        $stmt->bindParam(":id", $id);
         $stmt->execute();
-        standardizedResponse("Success", $stmt->fetchAll(PDO::FETCH_ASSOC));
+        standardizedResponse("Success", $stmt->fetch());
     } catch (Exception $e) {
         standardizedResponse($e->getMessage());
     }
